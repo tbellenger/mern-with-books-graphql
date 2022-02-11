0a615be62731d88ff1f6156aa3020ec26699a370
tbellenger <tbellenger@gmail.com>
2022-02-10 20:20:49 -0800

    fix me on server and add mutations and queries on client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $description: String!
+    $title: String!
+    $bookId: String!
+    $image: String!
+    $link: String!
+    $authors: [String!]!
+  ) {
+    saveBook(
+      description: $description
+      title: $title
+      bookId: $bookId
+      image: $image
+      link: $link
+      authors: $authors
+    ) {
+      username
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      username
+    }
+  }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,41 @@
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
